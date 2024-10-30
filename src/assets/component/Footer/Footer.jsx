e8b2569e3ccc632f5dd7f5ab72b08dd6a1c0af1f
Yen Wang <wang80139@gmail.com>
2024-10-30 14:09:31 +1300

    Finialized design

--- src/assets/component/Footer/Footer.jsx
+++ src/assets/component/Footer/Footer.jsx
@@ -1,5 +1,12 @@
+import style from './Footer.module.css'
+
 export default function Footer(){
     return(
-        <div>FOOTER</div>
+        <div className={style.footerContainer}>
+            <a href="https://www.manana.co.nz/">
+                <h1>Powered By</h1>
+                <img src='./FooterLogo.png' alt='Footer Logo' />
+            </a>
+        </div>
     )
 }
--- src/assets/component/SignUP/ConfirmMessage/ConfirmMessage.jsx
+++ src/assets/component/SignUP/ConfirmMessage/ConfirmMessage.jsx
@@ -1,5 +1,12 @@
+import style from './ConfirmMessage.module.css'
+
+
 export default function ConfirmMessage(){
     return(
-        <div>ConfirmMessage</div>
+        <div className={style.confirmMessageContainer}>
+            <h3>Thank you!</h3>
+            <p>Once you're successfully signed up to our newsletter, a confirmation email will be sent to your inbox.</p>
+            <img src="./check.png" alt="Check Sign" />
+        </div>
     )
 }
